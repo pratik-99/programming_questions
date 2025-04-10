c3924a2f31a7a21c7070c5e771c954759d519026
Pratik poojary <pratik.poojary@somaiya.edu>
2025-04-10 23:18:54 +0530

    Initial commit

---
+++ bijective_pattern.py
@@ -0,0 +1,52 @@
+"""
+Problem statement
+You are given a pattern in the form of a string and a collection of words. 
+Your task is to determine if the pattern string and the collection of words have the same order.
+
+Note :
+The strings are non-empty.
+
+Sample Input 1 :
+1
+abab 4
+bat ball bat ball
+Sample Output 1 :
+True
+
+Sample Input 2 :
+2
+abbb 4
+bat ball bat bat
+abab 4
+bat bat bat bat
+Sample Output 2 :
+False
+False
+
+The strings only contain lowercase English letters.
+"""
+def isPatternMatching(pattern, words):
+    # write your code here
+    # Return a boolean variable denoting the answer to the problem.
+
+    if len(pattern) != len(words):
+        return False
+    word_to_char={}
+    char_to_word={}
+    for char, word in zip(pattern,words):
+        if(char in char_to_word):
+            if(char_to_word[char]!=word):
+                print(char,word)
+                return False
+        else:
+            char_to_word[char]=word
+
+        if(word in word_to_char):
+            if(word_to_char[word]!=char):
+                print(char,word)
+                return False
+        else:
+            word_to_char[word]=char
+
+    return True
+print(isPatternMatching("abab",["bat", "ball", "bat", "ball"]))    
