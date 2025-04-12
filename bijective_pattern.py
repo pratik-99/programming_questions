dc8d64928cbf4ce6282e50ab7d6c6c150611cf0f
Pratik poojary <pratik.poojary@somaiya.edu>
2025-04-12 23:49:37 +0530

    added my solution

--- bijective_pattern.py
+++ bijective_pattern.py
@@ -25,6 +25,8 @@ False
 
 The strings only contain lowercase English letters.
 """
+from collections import defaultdict
+
 def isPatternMatching(pattern, words):
     # write your code here
     # Return a boolean variable denoting the answer to the problem.
@@ -49,4 +51,19 @@ def isPatternMatching(pattern, words):
             word_to_char[word]=char
 
     return True
-print(isPatternMatching("abab",["bat", "ball", "bat", "ball"]))    
+print("Solution 1",isPatternMatching("abab",["bat", "ball", "bat", "ball"]))    
+
+def isPatternMatchingMySolution(pattern, words):
+    pattern_matching_object=defaultdict(list)
+    for index, char in enumerate(pattern):
+        pattern_matching_object[char].append(index)
+        
+    item_match=defaultdict(list)
+    for index, item in enumerate(words):
+        item_match[item].append(index)
+    a_sorted = sorted([sorted(sublist) for sublist in pattern_matching_object.values()])
+    b_sorted = sorted([sorted(sublist) for sublist in item_match.values()])
+    
+    return a_sorted== b_sorted
+
+print("Solution 2",isPatternMatching("abab",["bat", "ball", "bat", "ball"]))    
